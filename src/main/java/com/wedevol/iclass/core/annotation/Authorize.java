87e3dd7c977670d000ce84d990b64d5f8ce129c7
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-02-16 20:02:51 -0300

    Authorization annotation, interceptor and configuration

---
+++ src/main/java/com/wedevol/iclass/core/annotation/Authorize.java
@@ -0,0 +1,20 @@
+package com.wedevol.iclass.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Authorize Annotation for the controllers
+ *
+ * @author Charz++
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authorize {
+
+	boolean basic() default false;
+
+	boolean hard() default false;
+}
---
+++ src/main/java/com/wedevol/iclass/core/configuration/InterceptorConfiguration.java
@@ -0,0 +1,21 @@
+package com.wedevol.iclass.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.wedevol.iclass.core.interceptor.AuthorizationInterceptor;
+
+/**
+ * Request interceptor configuration
+ *
+ * @author Charz++
+ */
+@Configuration
+public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuthorizationInterceptor());
+	}
+}
