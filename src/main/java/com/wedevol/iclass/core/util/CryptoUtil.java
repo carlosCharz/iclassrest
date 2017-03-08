9f181ecd8642e9c4459b7f31f27e96a116c6d959
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-03-08 14:43:04 -0300

    Add the randomString

--- src/main/java/com/wedevol/iclass/core/util/CryptoUtil.java
+++ src/main/java/com/wedevol/iclass/core/util/CryptoUtil.java
@@ -1,11 +1,12 @@
 package com.wedevol.iclass.core.util;
 
+import java.security.SecureRandom;
+import java.util.UUID;
+
 import com.fasterxml.uuid.EthernetAddress;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.RandomBasedGenerator;
 import com.fasterxml.uuid.impl.TimeBasedGenerator;
-
-import java.util.UUID;
 
 /**
  * Crypto Util Class
@@ -15,23 +16,34 @@ import java.util.UUID;
  */
 public class CryptoUtil {
 
-    private static TimeBasedGenerator TB_GENERATOR = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
-    private static RandomBasedGenerator RD_GENERATOR = Generators.randomBasedGenerator();
+	private static TimeBasedGenerator TB_GENERATOR = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
+	private static RandomBasedGenerator RD_GENERATOR = Generators.randomBasedGenerator();
+	private static final String AVAILABLE_LETTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private static SecureRandom RANDOM = new SecureRandom();
 
-    public static UUID getRandomBasedUUID() {
-        return RD_GENERATOR.generate();
-    }
+	public static UUID getRandomBasedUUID() {
+		return RD_GENERATOR.generate();
+	}
 
-    public static String getRandomBasedUUIDString() {
-        return getRandomBasedUUID().toString().replace("-","");
-    }
+	public static String getRandomBasedUUIDString() {
+		return getRandomBasedUUID().toString()
+									.replace("-", "");
+	}
 
-    public static UUID getTimeBasedUUID() {
-        return TB_GENERATOR.generate();
-    }
+	public static UUID getTimeBasedUUID() {
+		return TB_GENERATOR.generate();
+	}
 
-    public static String getTimeBasedUUIDString() {
-        return getTimeBasedUUID().toString().replace("-","");
-    }
+	public static String getTimeBasedUUIDString() {
+		return getTimeBasedUUID().toString()
+									.replace("-", "");
+	}
+
+	public static String randomString(Integer size) {
+		StringBuilder sb = new StringBuilder(size);
+		for (int i = 0; i < size; i++)
+			sb.append(AVAILABLE_LETTERS.charAt(RANDOM.nextInt(AVAILABLE_LETTERS.length())));
+		return sb.toString();
+	}
 
 }
