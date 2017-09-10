02f032e6dd495844310a5666013032a7da0bd6f0
Carlos Becerra <carlos.becerra1234@gmail.com>
2017-09-10 16:50:44 -0300

    problem with swagger

--- src/main/java/com/wedevol/iclass/core/configuration/SwaggerConfig.java
+++ src/main/java/com/wedevol/iclass/core/configuration/SwaggerConfig.java
@@ -32,7 +32,7 @@ public class SwaggerConfig {
 
 	private ApiInfo apiInfo() {
 		Contact contact = new Contact("Charz++", "http://iclassapp.com", "carlos.becerra1234@gmail.com");
-		ApiInfo apiInfo = new ApiInfo("iClass REST API", "App to manage classes", "2.0.0", null, contact, null, null, null);
+		ApiInfo apiInfo = new ApiInfo("iClass REST API", "App to manage classes", "2.0.0", null, contact, null, null);
 		return apiInfo;
 	}
 }
